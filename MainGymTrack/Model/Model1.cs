c1ac946cbf6dd95107241058a68f2136b16a34db
DeGuzman-Bryan <enderkidbryan@gmail.com>
2024-11-15 21:39:57 +0800

    Add project files.

---
+++ MainGymTrack/Model/Model1.cs
@@ -0,0 +1,14 @@
+﻿namespace MainGymTrack.Model
+{
+    public class Model1
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public string Program { get; set; }
+
+        public string AppointmentDate { get; set; }
+    }
+}
---
+++ MainGymTrack/Model/TraineeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MainGymTrack.Model
+{
+    public class TraineeContext : DbContext
+    {
+        public TraineeContext(DbContextOptions<TraineeContext> options) : base(options)
+        {
+           
+
+        }
+       public DbSet<Model1> model1 {  get; set; } 
+    }
+}
+
+            
+        
+    
