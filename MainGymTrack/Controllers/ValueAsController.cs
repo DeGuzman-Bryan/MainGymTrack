792ab7bbfa7b4144103b8d2dbb00d9add2d6433d
DeGuzman-Bryan <enderkidbryan@gmail.com>
2024-11-17 07:27:59 +0800

    CONTROLLERS UPDATED

--- MainGymTrack/Controllers/ValueAsController.cs
+++ MainGymTrack/Controllers/ValueAsController.cs
@@ -18,9 +18,9 @@ namespace MainGymTrack.Controllers
         }
 
         [HttpGet]
-        public  async Task <ActionResult<IEnumerable<Model1>>> GetModel()
+        public async Task<ActionResult<IEnumerable<Model1>>> GetModel()
         {
-            if(_dbContext.model1 == null)
+            if (_dbContext.model1 == null)
             {
                 return NotFound();
             }
@@ -39,19 +39,73 @@ namespace MainGymTrack.Controllers
             var model = await _dbContext.model1.FindAsync(id);
             if (model == null)
             {
-                return NotFound ();
-            } 
-            return model; 
+                return NotFound();
+            }
+            return model;
         }
-            [HttpPost]
-            public async Task<ActionResult<Model1>> PostModel (Model1 model)
-            {
-                _dbContext.model1.Add(model);
-                await _dbContext.SaveChangesAsync();
+        [HttpPost]
+        public async Task<ActionResult<Model1>> PostModel(Model1 model)
+        {
+            _dbContext.model1.Add(model);
+            await _dbContext.SaveChangesAsync();
 
-                return CreatedAtAction(nameof(GetModel), new { id = model.Id }, model) ;
+            return CreatedAtAction(nameof(GetModel), new { id = model.Id }, model);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutModel(int id, Model1 model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
             }
 
+            _dbContext.Entry(model).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModelAvailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
         }
+
+        private bool ModelAvailable(int id)
+        {
+            return (_dbContext.model1?.Any(x => x.Id == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModel(int id)
+        {
+            if (_dbContext.model1 == null)
+            {
+                return NotFound(); // Ensure the DbSet is not null
+            }
+
+            var model = await _dbContext.model1.FindAsync(id); 
+            if (model == null)
+            {
+                return NotFound(); 
+            }
+
+            _dbContext.model1.Remove(model); 
+            await _dbContext.SaveChangesAsync(); 
+            return Ok(); 
+        }
+
     }
-    
+}
+
+
